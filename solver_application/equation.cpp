d1e35ec473a77a6336cb23935eab91b4671f0a64
Maksim <78586711+Mursulov@users.noreply.github.com>
2025-06-01 16:34:47 +0300

    Update equation.cpp

--- solver_application/equation.cpp
+++ solver_application/equation.cpp
@@ -6,12 +6,13 @@ int main() {
     float a, b, c, x1, x2;
     std::cin >> a >> b >> c;
     float d = b * b - 4 * a * c;
-    if (d < 0) {
-        std::cout << "No real roots" << std::endl;
-    } else {
+    if (d < 0)
+        std::cout << "No real roots\n";
+    else {
         solve(a, b, c, x1, x2);
-        std::cout << "x1 = " << std::fixed << std::setprecision(5) << x1 << std::endl;
-        std::cout << "x2 = " << std::fixed << std::setprecision(5) << x2 << std::endl;
+        std::cout << std::fixed << std::setprecision(5)
+                  << "x1 = " << x1 << '\n'
+                  << "x2 = " << x2 << '\n';
     }
     return 0;
 }
